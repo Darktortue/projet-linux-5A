2e3254b862e1f9b9fcb72b45f03e8f16f457fa0c
Darktortue <11302315+Darktortue@users.noreply.github.com>
2021-10-22 17:50:57 +0200

    fix lot of stuff

--- keycloak_bookstack.sh
+++ keycloak_bookstack.sh
@@ -2,18 +2,16 @@
 
 ### Keycloak & Bookstack setup ######################################################################################################
 
-dnf install java-1.8.0-openjdk-devel -y
+dnf install wget java-1.8.0-openjdk-devel -y
 java -version
 useradd -s /sbin/nologin -r keycloak
 version=15.0.2
-mkdir -vp /opt/keycloak/{current,$version}
-wget https://github.com/keycloak/keycloak/releases/download/15.0.2/keycloak-15.0.2.tar.gz -P /opt/keycloak/${version}
-ln -s /opt/keycloak/$version /opt/keycloak/current
-tar -xzvf /opt/keycloak/${version}/keycloak-$version.tar.gz -C /opt/keycloak/current --strip-components=1
+cd /opt && wget https://github.com/keycloak/keycloak/releases/download/15.0.2/keycloak-15.0.2.tar.gz
+tar -xzf keycloak-15.0.2.tar.gz
 
 ### Database setup ##################################################################################################################
 
-mysql -uroot<<MYSQL_SCRIPT
+mysql -uroot -pma62s1l6Bd7EXA<<MYSQL_SCRIPT
 CREATE DATABASE keycloak CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'%' identified by 'toto';
 GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'localhost' identified by 'toto';
@@ -22,7 +20,7 @@ MYSQL_SCRIPT
 
 wget https://cdn.mysql.com//Downloads/Connector-J/mysql-connector-java-8.0.26.tar.gz
 mkdir -vp /opt/keycloak/current/modules/system/layers/keycloak/com/mysql/main
-tar -xzvf mysql-connector-java-8.0.26.tar.gz -C /opt/keycloak/current/modules/system/layers/keycloak/com/mysql/main
+tar -xzf mysql-connector-java-8.0.26.tar.gz -C /opt/keycloak/current/modules/system/layers/keycloak/com/mysql/main
 
 cat << '_EOF_' > /opt/keycloak/current/modules/system/layers/keycloak/com/mysql/main/module.xml
 <?xml version="1.0" ?>
@@ -93,7 +91,7 @@ After=network.target
 Type=idle
 User=keycloak
 Group=keycloak
-ExecStart=/opt/keycloak/current/bin/standalone.sh -b 0.0.0.0
+ExecStart=/opt/keycloak-15.0.2/bin/standalone.sh -b 0.0.0.0
 TimeoutStartSec=600
 TimeoutStopSec=600
  
@@ -102,6 +100,12 @@ WantedBy=multi-user.target
 _EOF_
 
 ### Keycloak nginx ##################################################################################################################
+
+cd /etc/nginx/certificats || exit
+openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
+    -subj "/C=FR/ST=Paris/L=Paris/O=ESGI/CN=sso.esgi.local" \
+    -keyout keycloak.key \
+    -out keycloak.crt
 
 cat << '_EOF_' > /etc/nginx/conf.d/keycloak.conf
 upstream keycloak {
@@ -138,7 +142,6 @@ server {
     ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AE;
     ssl_prefer_server_ciphers on;
 	ssl_session_cache shared:SSL:1m;
-	ssl_prefer_server_ciphers on;
 	
 	ssl_dhparam /etc/nginx/dhparam.pem;
 	
@@ -161,10 +164,10 @@ nginx -t
 systemctl restart nginx.service
 systemctl --no-pager status nginx.service
 
-opt/keycloak/current/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)'
-opt/keycloak/current/bin/jboss-cli.sh 'embed-server,/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=443)'
-opt/keycloak/current/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)'
-chown -R keycloak:keycloak /opt/keycloak
+/opt/keycloak-15.0.2/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)'
+/opt/keycloak-15.0.2/bin/jboss-cli.sh 'embed-server,/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=443)'
+/opt/keycloak-15.0.2/bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)'
+chown -R keycloak:keycloak /opt/keycloak-15.0.2
 #firewall-cmd --zone=public --add-port=8080/tcp
 systemctl daemon-reload
 systemctl enable keycloak.service
@@ -173,12 +176,15 @@ systemctl --no-pager status keycloak.service
 
 ### Keycloak setup ##################################################################################################################
 
-opt/keycloak/current/bin/add-user-keycloak.sh -u admin -p password -r master
+/opt/keycloak-15.0.2/bin/add-user-keycloak.sh -u admin -p password -r master
 systemctl restart keycloak.service
+echo "Waiting for the keycloak service to be up..."
+sleep 5
 systemctl --no-pager status keycloak.service
-opt/keycloak/current/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password password
-opt/keycloak/current/bin/kcadm.sh create realms -s realm=KOLLAB -s enabled=true
-opt/keycloak/current/bin/add-user-keycloak.sh -r KOLLAB -u nimda -p password
+
+/opt/keycloak-15.0.2/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password password
+/opt/keycloak-15.0.2/bin/kcadm.sh create realms -s realm=chat -s enabled=true
+/opt/keycloak-15.0.2/bin/add-user-keycloak.sh -r chat -u nimda -p password
 systemctl restart keycloak.service
 systemctl --no-pager status keycloak.service
 
@@ -228,6 +234,7 @@ SAML2_REMOVE_FROM_GROUPS=false
 #APP_DEBUG=true
 _EOF_
 
-echo "Don't forget to add the SAML2_IDP_x509 secret !!"
+echo "\nDon't forget to add the SAML2_IDP_x509 secret !!"
+echo
 
 sleep 2
